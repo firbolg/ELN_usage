1aac62a7efd726871f42fac0637978b88cdbd917
firbolg <levijdolan@gmail.com>
2022-08-02 10:43:50 -0400

    loop through files with summed variables

--- x.py
+++ x.py
@@ -4,8 +4,9 @@ from collections import Counter
 import pandas as pd
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
+import glob
 
-path = r"C:\Users\Levi\projects\ELN_usage\2021_test"
+#path = "C:\Users\Levi\projects\ELN_usage\2021_test"
 
 df = pd.read_excel((r'C:\Users\Levi\projects\ELN_usage\2021_test\August_2021_User_Stats.xlsx'))
 
@@ -14,5 +15,20 @@ activities = df['Total Activities'].sum()
 data_use = df['MB Used'].sum()
 notebooks = df['Notebooks Owned'].sum()
 logins = df['Total Logins'].sum()
-print(df.head(), "activities:", activities, "data used:", data_use, 
-"notebooks:", notebooks, "logins:", logins)
+
+path = r"C:\Users\Levi\projects\ELN_usage\2021_test"
+csv_files = glob.glob(os.path.join(path, "*.xlsx"))
+  
+  
+# loop over the list of csv files
+for f in csv_files:
+    
+    # read the csv file
+    df = pd.read_excel(f)
+      
+    activities = df['Total Activities'].sum()
+    data_use = df['MB Used'].sum()
+    notebooks = df['Notebooks Owned'].sum()
+    logins = df['Total Logins'].sum()
+    print(activities, data_use, notebooks, logins)
+  
