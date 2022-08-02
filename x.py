23533d5e53c095d2788dfd62e92dda00d0758b58
firbolg <levijdolan@gmail.com>
2022-08-02 11:17:25 -0400

    split month and year into columns

--- x.py
+++ x.py
@@ -6,29 +6,25 @@ from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 import glob
 
-#path = "C:\Users\Levi\projects\ELN_usage\2021_test"
-
-df = pd.read_excel((r'C:\Users\Levi\projects\ELN_usage\2021_test\August_2021_User_Stats.xlsx'))
-
-
-activities = df['Total Activities'].sum()
-data_use = df['MB Used'].sum()
-notebooks = df['Notebooks Owned'].sum()
-logins = df['Total Logins'].sum()
 
 path = r"C:\Users\Levi\projects\ELN_usage\2021_test"
 csv_files = glob.glob(os.path.join(path, "*.xlsx"))
   
-  
+sums = []  
 # loop over the list of csv files
 for f in csv_files:
     
     # read the csv file
     df = pd.read_excel(f)
-      
+
+    name = f.split("\\")[-1]  
+    name = name.split("_")
     activities = df['Total Activities'].sum()
     data_use = df['MB Used'].sum()
     notebooks = df['Notebooks Owned'].sum()
     logins = df['Total Logins'].sum()
-    print(activities, data_use, notebooks, logins)
-  
+    sums.append((name[0],name[1],activities,data_use,notebooks,logins))
+
+df2 = pd.DataFrame(sums, columns=('Month','Year','Activities','Data Usage','Notebooks','Logins')) 
+#df2['Name'] = pd.Categorical(df['Name'], categories=months, ordered=True)
+print(df2)
